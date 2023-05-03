665f69062eeab4fadbf19221cb98c2af9a64bac2
Cococabj <130006194+Cococabj@users.noreply.github.com>
2023-05-03 09:11:38 -0300

    Header y Footer sincronizado

---
+++ JavaScript/header.js
@@ -0,0 +1,22 @@
+document.write(`
+
+<div  id="inicio" style="background-color: #F8C303;">
+<a href="#inicio"><img src="/IMG/bicicleta.png" alt="Inicio" width="150"></a>
+<h1>Codo a Codo Bikes</h1>
+<a href="#inicio"><img src="/IMG/bicicleta.png" alt="Inicio" width="150"></a>
+
+</div>
+<div >
+
+<nav id="pruebanav">
+    <li><a href="./index.html">Inicio</a></li>
+    <li><a href="#alquila">Alquila Ahora</a></li>
+    <li><a href="#servicios">Servicios</a></li>
+    <li><a href="./nosotros.html">Nosotros</a></li>
+    <li><a href="#contacto">Contacto</a></li>
+    <li><a href="#sucursales">Sucursales</a></li>
+
+</nav>
+</div>
+
+`);
