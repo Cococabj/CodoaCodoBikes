ca594da27e4f8741e3bb6881c7645dc8a9c68a76
Cococabj <130006194+Cococabj@users.noreply.github.com>
2023-05-03 18:07:00 -0300

    commit

--- JavaScript/header.js
+++ JavaScript/header.js
@@ -3,9 +3,9 @@ document.write(`
 <h2>Este es un header sincronizado en header.JS</h2>
 <div  id="inicio" style="background-color: #F8C303;">
 
-    <a href="#inicio"><img src="/IMG/bicicleta.png" alt="Inicio" width="150"></a>
+    <a href="#inicio"><img src="./img/bicicleta.png" alt="Inicio" width="150"></a>
     <h1>Codo a Codo Bikes</h1>
-    <a href="#inicio"><img src="/IMG/bicicleta.png" alt="Inicio" width="150"></a>
+    <a href="#inicio"><img src="./img/bicicleta.png" alt="Inicio" width="150"></a>
 
 </div>
 
