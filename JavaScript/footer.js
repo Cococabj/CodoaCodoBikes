7bd13ca3a931ea8f3b3c41180fa8218dd2bc42f5
Cococabj <130006194+Cococabj@users.noreply.github.com>
2023-05-03 09:28:05 -0300

    cambios menores

--- JavaScript/footer.js
+++ JavaScript/footer.js
@@ -2,10 +2,7 @@ document.write(`
 
 <div  id="inicio" style="background-color: #F8C303;">
     <div style="background-color: #F8C303;">
-        <h2>Hola soy un footer</h2>
-        <h3>prueba</h3>
-        <h3>segunda prueba</h3>
-        <h3> tercer y ultima</h3>
+        <h2>Hola soy un footer de prueba en footer.JS</h2>
     </div>
 </div>
 
--- JavaScript/header.js
+++ JavaScript/header.js
@@ -1,22 +1,25 @@
 document.write(`
 
+<h2>Este es un header sincronizado en header.JS</h2>
 <div  id="inicio" style="background-color: #F8C303;">
-<a href="#inicio"><img src="/IMG/bicicleta.png" alt="Inicio" width="150"></a>
-<h1>Codo a Codo Bikes</h1>
-<a href="#inicio"><img src="/IMG/bicicleta.png" alt="Inicio" width="150"></a>
+
+    <a href="#inicio"><img src="/IMG/bicicleta.png" alt="Inicio" width="150"></a>
+    <h1>Codo a Codo Bikes</h1>
+    <a href="#inicio"><img src="/IMG/bicicleta.png" alt="Inicio" width="150"></a>
 
 </div>
-<div >
 
-<nav id="pruebanav">
-    <li><a href="./index.html">Inicio</a></li>
-    <li><a href="#alquila">Alquila Ahora</a></li>
-    <li><a href="#servicios">Servicios</a></li>
-    <li><a href="./nosotros.html">Nosotros</a></li>
-    <li><a href="#contacto">Contacto</a></li>
-    <li><a href="#sucursales">Sucursales</a></li>
-
-</nav>
+<div>
+    <div>
+        <nav id="pruebanav">
+            <li><a href="./index.html">Inicio</a></li>
+            <li><a href="#alquila">Alquila Ahora</a></li>
+            <li><a href="#servicios">Servicios</a></li>
+            <li><a href="./nosotros.html">Nosotros</a></li>
+            <li><a href="#contacto">Contacto</a></li>
+            <li><a href="#sucursales">Sucursales</a></li>
+        </nav>
+    </div>
 </div>
 
 `);
