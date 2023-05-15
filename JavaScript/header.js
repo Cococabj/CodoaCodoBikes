ab28a02fb00093e7a7bc0de35f844b8f5e02f2fd
Cococabj <130006194+Cococabj@users.noreply.github.com>
2023-05-15 18:43:53 -0300

    .

--- JavaScript/header.js
+++ JavaScript/header.js
@@ -1,25 +1,33 @@
 document.write(`
 
-<h2>Este es un header sincronizado en header.JS hola</h2>
-<div  id="inicio" style="background-color: #F8C303;">
+<h2>Este es un header sincronizado en header.JS hola prueba</h2>
+<div id="inicio" style="background-color: #F8C303;">
+
 
     <a href="#inicio"><img src="./img/bicicleta.png" alt="Inicio" width="150"></a>
     <h1>Codo a Codo Bikes</h1>
     <a href="#inicio"><img src="./img/bicicleta.png" alt="Inicio" width="150"></a>
 
 </div>
-
 <div>
-    <div>
-        <nav id="pruebanav">
+    <nav id="pruebanav">
+        <input type="checkbox" id="menu">
+        <label for="menu"> ☰ </label>
+        <ul>
             <li><a href="./index.html">Inicio</a></li>
             <li><a href="./alquila.html">Alquila Ahora</a></li>
-            <li><a href="./recorridos.html">Recorridos</a></li>
+            <li><a href="./recorridos.html">Servicios</a></li>
             <li><a href="./nosotros.html">Nosotros</a></li>
-            <li><a href="./index.html#contacto">Contacto</a></li>
-            <li><a href="./index.html#sucursales">Sucursales</a></li>
-        </nav>
-    </div>
+            <li><a href="#contacto">Contacto</a></li>
+            <li><a href="#sucursales">Sucursales</a></li>
+        </ul>
+        <li><a href="./index.html">Inicio</a></li>
+        <li><a href="./alquila.html">Alquila Ahora</a></li>
+        <li><a href="./recorridos.html">Servicios</a></li>
+        <li><a href="./nosotros.html">Nosotros</a></li>
+        <li><a href="#contacto">Contacto</a></li>
+        <li><a href="#sucursales">Sucursales</a></li>
+    </nav>
 </div>
 
 `);
