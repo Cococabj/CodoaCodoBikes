a9c9585161752b6c6f29a7828091a1e6acb6e177
Cococabj <130006194+Cococabj@users.noreply.github.com>
2023-05-23 09:02:02 -0300

    header,footer,index,alquila y recorrido corregidos

--- JavaScript/footer.js
+++ JavaScript/footer.js
@@ -1,9 +1,46 @@
 document.write(`
 
-<div  id="inicio" style="background-color: #F8C303;">
-    <div style="background-color: #F8C303;">
-        <h2>Hola soy un footer de prueba en footer.JS</h2>
-    </div>
-</div>
+<div id="footer">
+                <div class="footer-container">
+                    <div class="redes">
+                        <p>Podes seguirnos en:</p>
+                        <a href="https://www.instagram.com"><img src="./img/instagram.png" alt="www.instagram.com"
+                                width="25"></a>
+                        <a href="https://www.facebook.com"><img src="./img/facebook.png" alt="www.facebook.com"
+                                width="25"></a>
+                        <a href="https://www.twitter.com"><img src="./img/twitter.png" alt="www.twitter.com"
+                                width="25"></a>
+                        <a href="https://www.youtube.com"><img src="./img/youtube.png" alt="www.youtube.com"
+                                width="25"></a>
+                        <p>Tambien podes encontrarnos en Whats App al 11-2222-3333<span><a class="whats-app"
+                                    href="https://api.whatsapp.com/send?phone="><img src="./img/whatsapp.png" alt=""
+                                        width="35"></a></span></p>
+                    </div>
+                    <div class="desarrollo">
+                        <p>Codo a Codo Bikes esta desarrolado con:</p>
+                        <a href="https://www.w3schools.com/html/"><img src="./img/html.png" alt="HTML" width="25"></a>
+                        <a href="https://www.w3schools.com/w3css/default.asp"><img src="./img/css.png" alt="CSS"
+                                width="25"></a>
+                        <a href="https://www.w3schools.com/js/"><img src="./img/js.png" alt="JavaScript" width="25"></a>
+                    </div>
+                    <div class="creacion">
+                        <p>Creado por:</p>
+                        <p>
+                            <span>
+                                Leandro Pares,
+                            </span>
+                            <span>
+                                Carolina Canteros
+                            </span>
+                            <span>
+                                y
+                            </span>
+                            <span>
+                                Tomas Di Chiara
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
 
 `);
