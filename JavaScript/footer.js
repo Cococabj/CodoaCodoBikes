bc6df53e7d28dfc0d2ae6b0648b328209de2b751
Cococabj <130006194+Cococabj@users.noreply.github.com>
2023-07-12 04:51:45 -0300

    corregido footer y botones que no funcionaban

--- JavaScript/footer.js
+++ JavaScript/footer.js
@@ -1,59 +1,64 @@
 document.write(`
 
 <div id="footer">
-                <div class="footer-container">
+            <div class="footer-container">
                 <div class="local">
-                <br>
-                    <p>Nuestra sede central esta en 
-                    <a href="https://goo.gl/maps/GxFxn6baeFy9AKbs5">
-                    Av. Lope de Vega 326</a></p>
+                    <p>Nuestra sede central esta en
+                        <a href="https://goo.gl/maps/GxFxn6baeFy9AKbs5">
+                            Av. Lope de Vega 326</a>
+                    </p>
                     <p>Podes ver y probar los modelos disponibles antes de alquilarlos!</p>
-                <br>
                 </div>
+                <div class="footer-flex ">
                     <div class="redes">
                         <div class="redes">
-                        <p>Podes seguirnos en:</p>
+                            <p>Podes seguirnos en: </p>
+                                <a href="https://www.instagram.com"><img src="./img/instagram.png"
+                                        alt="www.instagram.com" width="25"></a>
+                                <a href="https://www.facebook.com"><img src="./img/facebook.png" alt="www.facebook.com"
+                                        width="25"></a>
+                                <a href="https://www.twitter.com"><img src="./img/twitter.png" alt="www.twitter.com"
+                                        width="25"></a>
+                                <a href="https://www.youtube.com"><img src="./img/youtube.png" alt="www.youtube.com"
+                                        width="25"></a>
+                            <p>Estamos en Whats App:
+                                <a class="whats-app" href="https://api.whatsapp.com/send?phone=11-1111-1111">
+                                    <img src="./img/whatsapp.png" alt="https://api.whatsapp.com/send?phone=11-1111-1111"
+                                        width="25">
+                                </a>
+                            </p>
                         </div>
-                        <div class="redes">
-                        <a href="https://www.instagram.com"><img src="./img/instagram.png" alt="www.instagram.com"
-                                width="25"></a>
-                        <a href="https://www.facebook.com"><img src="./img/facebook.png" alt="www.facebook.com"
-                                width="25"></a>
-                        <a href="https://www.twitter.com"><img src="./img/twitter.png" alt="www.twitter.com"
-                                width="25"></a>
-                        <a href="https://www.youtube.com"><img src="./img/youtube.png" alt="www.youtube.com"
-                                width="25"></a>
-                        </div>
-                        <p>Tambien podes encontrarnos en Whats App al 11-1111-1111 <a class="whats-app"
-                                    href="https://api.whatsapp.com/send?phone=11-1111-1111"><img src="./img/whatsapp.png" alt="https://api.whatsapp.com/send?phone=11-1111-1111"
-                                        width="35"></a></p>
                     </div>
                     <div class="desarrollo">
-                        <p>Codo a Codo Bikes esta desarrolado con:</p>
-                        <a href="https://www.w3schools.com/html/"><img src="./img/html.png" alt="HTML" width="25"></a>
-                        <a href="https://www.w3schools.com/w3css/default.asp"><img src="./img/css.png" alt="CSS"
-                                width="25"></a>
-                        <a href="https://www.w3schools.com/js/"><img src="./img/js.png" alt="JavaScript" width="25"></a>
-                        <a href="https://www.w3schools.com/python/"><img src="./img/python.png" alt="Python" width="25"></a>
-                    </div>
-                    <div class="creacion">
-                        <p>Creado por:</p>
-                        <p>
-                            <span>
-                                Leandro Pares,
-                            </span>
-                            <span>
-                                Carolina Canteros
-                            </span>
-                            <span>
-                                y
-                            </span>
-                            <span>
-                                Tomas Di Chiara
-                            </span>
-                        </p>
+                        <p>Codo a Codo Bikes fue desarrolado usando:</p>
+                            <a href="https://www.w3schools.com/html/"><img src="./img/html.png" alt="HTML"
+                                    width="25"></a>
+                            <a href="https://www.w3schools.com/w3css/default.asp"><img src="./img/css.png" alt="CSS"
+                                    width="25"></a>
+                            <a href="https://www.w3schools.com/js/"><img src="./img/js.png" alt="JavaScript"
+                                    width="25"></a>
+                            <a href="https://www.w3schools.com/python/"><img src="./img/python.png" alt="Python"
+                                    width="25"></a>
+                        
                     </div>
                 </div>
+                <div class="creacion">
+                    <p>Creado por:
+                        <span>
+                            Leandro Pares,
+                        </span>
+                        <span>
+                            Carolina Canteros
+                        </span>
+                        <span>
+                            y
+                        </span>
+                        <span>
+                            Tomas Di Chiara
+                        </span>
+                    </p>
+                </div>
             </div>
+        </div>
 
 `);
